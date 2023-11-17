c20683c76a47d6c1d242f42af54a8a0fdf32edcd
Alfred Peter <alpeter91@gmail.com>
2023-11-17 21:20:20 +0300

    Add mobile menu layout and styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,102 @@
-import { NavLink } from "react-router-dom";
+import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import {
+  FaBars,
+  FaX,
+  FaGithub,
+  FaLinkedinIn,
+  FaXTwitter,
+  FaMedium,
+} from 'react-icons/fa6';
+import './navbar.scss';
 
 const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
+
   return (
-    <nav>
-      <h1>Alfred</h1>
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='about'>About</NavLink>
-      <NavLink to='portfolio'>Portfolio</NavLink>
-      <NavLink to='contact'>Contact</NavLink>
-    </nav>
-  )
-}
-export default Navbar
+    <>
+      <nav className={showNav ? '' : 'invisible'}>
+        <div className='close-menu'>
+          <FaX onClick={() => setShowNav(false)} />
+        </div>
+
+        <div id='logo'>
+          <h1>
+            <Link to='/' onClick={() => setShowNav(false)}>
+              {'<A/FRED>'} 
+            </Link>
+          </h1>
+        </div>
+
+        <div id='nav-links'>
+          <NavLink to='/' className='navlink' onClick={() => setShowNav(false)}>
+            Home
+          </NavLink>
+
+          <NavLink
+            to='about'
+            className='navlink'
+            onClick={() => setShowNav(false)}
+          >
+            About
+          </NavLink>
+
+          <NavLink
+            to='portfolio'
+            className='navlink'
+            onClick={() => setShowNav(false)}
+          >
+            Portfolio
+          </NavLink>
+
+          <NavLink
+            to='contact'
+            className='navlink'
+            onClick={() => setShowNav(false)}
+          >
+            Contact
+          </NavLink>
+        </div>
+
+        <div id='social-links'>
+          <a
+            target='_blank'
+            rel='noreferer'
+            href='https://linkedin.com/in/alfredmkg'
+          >
+            <FaLinkedinIn />
+          </a>
+
+          <a
+            target='_blank'
+            rel='noreferer'
+            href='https://github.com/badger-99'
+          >
+            <FaGithub />
+          </a>
+
+          <a
+            target='_blank'
+            rel='noreferer'
+            href='https://twitter.com/AlfredMkg'
+          >
+            <FaXTwitter />
+          </a>
+
+          <a
+            target='_blank'
+            rel='noreferer'
+            href='https://medium.com/@alfred.mkg'
+          >
+            <FaMedium />
+          </a>
+        </div>
+      </nav>
+
+      <div className={showNav ? 'invisible' : 'open-menu'}>
+        <FaBars onClick={() => setShowNav(true)} />
+      </div>
+    </>
+  );
+};
+export default Navbar;
