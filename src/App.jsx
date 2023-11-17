917383c6d2ed0bff7bc6ea9128c8ecea14537a83
Alfred Peter <alpeter91@gmail.com>
2023-11-17 21:22:40 +0300

    Move nav menu from Layout.jsx to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./Pages/Layout/Layout";
-import Home from "./Pages/Home/Home";
-import About from "./Pages/About/About";
-import Portfolio from "./Pages/Portfolio/Portfolio";
-import Contact from "./Pages/Contact/Contact";
-import './app.scss'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Layout from './Pages/Layout/Layout';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Portfolio from './Pages/Portfolio/Portfolio';
+import Contact from './Pages/Contact/Contact';
+import './app.scss';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="portfolio" element={<Portfolio />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path='about' element={<About />} />
+          <Route path='portfolio' element={<Portfolio />} />
+          <Route path='contact' element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
-}
-export default App
+  );
+};
+export default App;
