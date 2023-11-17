e08519bc896237f3f75e890c5c77c8c4eef7bbd0
Alfred Peter <alpeter91@gmail.com>
2023-11-17 16:22:20 +0300

    Add navbar and nested routes to layout

--- src/Pages/Layout/Layout.jsx
+++ src/Pages/Layout/Layout.jsx
@@ -1,6 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <section>
+      <Navbar />
+      <Outlet />
+    </section>
   )
 }
 export default Layout
