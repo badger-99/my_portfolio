9567789fb5d1777ed23e4361a7dcad0eb12b7e2b
Alfred Peter <alpeter91@gmail.com>
2023-11-17 15:53:25 +0300

    change css files to scss

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
+import './app.scss'
 
 const App = () => {
   return (
