e223677228a880e8ef5e73f4631c5a1ad8d5eef1
Alfred Peter <alpeter91@gmail.com>
2023-11-17 16:12:25 +0300

    Separate pages from components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout/Layout";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Portfolio from "./components/Portfolio/Portfolio";
-import Contact from "./components/Contact/Contact";
+import Layout from "./Pages/Layout/Layout";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import Contact from "./Pages/Contact/Contact";
 import './app.scss'
 
 const App = () => {
