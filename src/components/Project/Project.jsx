52a15037f5661bb5d995dc1924f1f0ac7783248b
Alfred Peter <alpeter91@gmail.com>
2023-11-18 14:03:38 +0300

    Add event listener to projects in mobile only

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,6 +1,31 @@
-const Project = ({Cover, Title, Description }) => {
+import { useState, useEffect } from 'react';
+
+const Project = ({ Cover, Title, Description }) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth < 768);
+  };
+
+  useEffect(() => {
+    // Add the event listener when the component mounts
+    window.addEventListener('resize', handleResize);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const handleClick = () => {
+    if (isMobile) {
+      // Render or perform actions for mobile
+      alert('Mobile click');
+    }
+  };
+
   return (
-    <div className='project'>
+    <div className='project' onClick={handleClick}>
       <img src={Cover} alt={Title} className='screenshot' />
       <div className='project-details'>
         <h4 className='title'>{Title}</h4>
@@ -9,4 +34,4 @@ const Project = ({Cover, Title, Description }) => {
     </div>
   );
 };
-export default Project
+export default Project;
