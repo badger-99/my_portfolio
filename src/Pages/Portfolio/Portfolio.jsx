9546248fee20bb22e654b423eb67bb29ebd1bff6
Alfred Peter <alpeter91@gmail.com>
2023-11-18 13:08:20 +0300

    Display projects on portfolio page

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,6 +1,48 @@
+import portfolioData from '../../data/portfolio.json'
+import './portfolio.scss'
+
 const Portfolio = () => {
+
+    const renderPortfolio = (portfolio) => {
+      return (
+        <div id='projects'>
+          {portfolio.map((project) => {
+            return (
+              <div className='project' key={project.id}>
+                <img
+                  src={project.Cover}
+                  alt={project.Title}
+                  className='screenshot'
+                />
+                <div className='project-details'>
+                  <h4 className='title'>{project.Title}</h4>
+                  <p className='description'>{project.Description}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      );
+    };
+  
   return (
-    <div>Portfolio</div>
-  )
+    <section id='portfolio'>
+      <h1>This is my work</h1>
+        {renderPortfolio(portfolioData.portfolio)}
+    </section>
+  );
+};
+export default Portfolio;
+
+{
+  /* <div className='project-details'>
+  <p className='title'>{project.Title}</p>
+  <h4 className='description'>{project.Description}</h4>
+  <button
+    className='btn'
+    onClick={() => window.open(project.Demo)}
+  >
+    View
+  </button>
+</div> */
 }
-export default Portfolio
