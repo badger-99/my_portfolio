5cf34932e266c3bd3c6c80f46902e1250ed5f00b
Alfred Peter <alpeter91@gmail.com>
2023-12-27 15:16:41 +0300

    Add contact form

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,8 +1,23 @@
+import './contact.scss'
+
 const Contact = () => {
   return (
-    <section className="pages" id="contact">
+    <section className='pages' id='contact'>
       <h1>Contact</h1>
+      <p>
+        I'm always interested in hearing about new projects, so if you'd like to have a
+        chat,  please don't hesitate to get in touch.
+      </p>
+      <div className='contact-form'>
+        <form>
+          <input type='text' name='name' placeholder='Name' required />
+          <input type='email' name='email' placeholder='Email' required />
+          <input type='text' name='Subject' placeholder='Subject' required />
+          <textarea name='message' placeholder='Message' required></textarea>
+          <input type='submit' className='flat-button' value='SEND' />
+        </form>
+      </div>
     </section>
-  )
+  );
 }
 export default Contact
