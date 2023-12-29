e7f82d847d9fbb73db7fa417d5b30f49bcb9f596
Alfred Peter <alpeter91@gmail.com>
2023-12-29 13:56:22 +0300

    Add Home page content

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <section className="pages" id="home">
-      <h1>Home</h1>
+    <section className='pages' id='home'>
+      <h1>Hi</h1>
+      <h1>I'm Alfred</h1>
+      <h2>A full stack developer</h2>
+      <p>
+        I can help you build a product, feature, or website. Feel free to look
+        through some of
+        <Link to='/portfolio'> my work</Link>, and if you like what you see and
+        have a project you need coded, don't hesitate to <Link to='/contact'> contact me</Link>.
+      </p>
     </section>
-  )
+  );
 }
 export default Home
