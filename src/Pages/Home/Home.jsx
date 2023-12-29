f193bb59f94af94ad331ec38851f9ebff5ef63b6
Alfred Peter <alpeter91@gmail.com>
2023-12-29 20:42:21 +0300

    Add About Me content

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
       <h1>Hi</h1>
       <h1>I'm Alfred</h1>
       <h2>A full stack developer</h2>
+      <br />
       <p>
         I can help you build a product, feature, or website. Feel free to look
         through some of
