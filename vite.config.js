748fd7055ddf62593c433a387757081042544705
Alfred Peter <alpeter91@gmail.com>
2023-12-29 23:43:44 +0300

    Update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  };
+
+  // We have a conditional config based on our Vite command
+  // so that when we are running 'npm run dev',
+  // the page will start serving on path http://localhost:5173/
+  // instead of http://localhost:5173/my_portfolio/
+
+  if (command !== 'serve') {
+    config.base = '/my_portfolio/';
+  }
+
+  return config;
+});
