50a36e86e4c49f5158b6de8a0b63576178c8348f
Alfred Peter <alpeter91@gmail.com>
2023-12-30 16:37:07 +0300

    Add 'basename' prop to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/my_portfolio/'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
