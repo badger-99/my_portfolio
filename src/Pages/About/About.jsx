e0c529aa909a661155a8bd62964c3c68ce743be3
Alfred Peter <alpeter91@gmail.com>
2024-01-03 01:16:15 +0300

    Update About Me content

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,20 +4,29 @@ import './about.scss'
 const About = () => {
   return (
     <section className='pages' id='about'>
-      <div id="header">
-      <h1>About Me</h1>
-      <Link to='/resume' id='link'>My Resumé</Link>
+      <div id='header'>
+        <h1>About Me</h1>
+        <Link to='/resume' id='link'>
+          My Resumé
+        </Link>
       </div>
 
       <p>
-        Hello there! My name is Alfred, but you can call me Al. When I'm not
-        dealing with code or reading, you'll most likely find me either trying
-        or modifying a recipe, volunteering somewhere, enjoying a
-        sci-fi show or film, or looking for a good rush that doesn't
-        involve bugs and a hard deadline.
+        I enjoy creating front and back-end components that integrate
+        seamlessly over a nice cup of coffee, and sometimes with some music
+        playing in the background. 
       </p>
       <br />
-      <p>Most of these activities will be conducted with background music and/or coffee.</p>
+      <p>
+        Among my positive qualities, my curiosity ranks high. It's the reason I can't walk away from something new without learning it or attempting to do it, and it's the reason I took an interest in programming.
+      </p>
+      <br />
+      <p>
+        When I'm not dealing with code or reading, you'll most likely find me
+        either trying or modifying a recipe, volunteering somewhere, enjoying a
+        sci-fi show or film, or looking for a good rush that doesn't involve
+        bugs and a hard deadline.
+      </p>
       <br />
     </section>
   );
