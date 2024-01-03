c5aa4352a0917c6163fc362288b1331accc0127d
Alfred Peter <alpeter91@gmail.com>
2024-01-03 23:25:29 +0300

    Add devicons

--- src/components/cube/Cube.jsx
+++ src/components/cube/Cube.jsx
@@ -5,27 +5,27 @@ const Cube = () => {
     <div className='stage-cube-cont'>
       <div className='cube-spinner'>
         <div className='face1'>
-          <FontAwesomeIcon icon={} color='#f06529' />
+          <i class='devicon-javascript-plain colored'></i>
         </div>
 
         <div className='face2'>
-          <FontAwesomeIcon icon={} color='#28a4d9' />
+          <i class='devicon-react-original colored'></i>
         </div>
 
         <div className='face3'>
-          <FontAwesomeIcon icon={} color='#efd81d' />
+          <i class='devicon-redux-original colored'></i>
         </div>
 
         <div className='face4'>
-          <FontAwesomeIcon icon={} color='#5ed4f4' />
+          <i class='devicon-ruby-plain colored'></i>
         </div>
 
         <div className='face5'>
-          <FontAwesomeIcon icon={} color='#ec4d28' />
+          <i class='devicon-rails-plain colored'></i>
         </div>
 
         <div className='face6'>
-          <FontAwesomeIcon icon={} color='#c69' />
+          <i class='devicon-postgresql-plain colored'></i>
         </div>
       </div>
     </div>
