49fa581c367cf1ae1f26be00a23feab243c50f18
Alfred Peter <alpeter91@gmail.com>
2024-01-04 20:58:53 +0300

    Change resume link position

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -8,25 +8,27 @@ const About = () => {
     <section className='pages' id='about'>
       <div id='header'>
         <h1>About Me</h1>
-        <Link to='/resume' id='link'>
-          My Resumé
-        </Link>
       </div>
-      <div id="text">
-      <p>
-        I enjoy creating front and back-end components that integrate
-        seamlessly over a nice cup of coffee, and sometimes with some music
-        playing in the background. 
-      </p>
-      <p>
-        Among my positive qualities, my curiosity ranks high. It's the reason I can't walk away from something new without learning it or attempting to do it, and it's the reason I took an interest in programming.
-      </p>
-      <p>
-        When I'm not dealing with code or reading, you'll most likely find me
-        either trying or modifying a recipe, volunteering somewhere, enjoying a
-        sci-fi show or film, or looking for a good rush that doesn't involve
-        bugs and a hard deadline.
-      </p>
+      <div id='text'>
+        <p>
+          I enjoy creating front and back-end components that integrate
+          seamlessly over a nice cup of coffee, and sometimes with some music
+          playing in the background.
+        </p>
+        <p>
+          Among my positive qualities, my curiosity ranks high. It's the reason
+          I can't walk away from something new without learning it or attempting
+          to do it, and it's the reason I took an interest in programming.
+        </p>
+        <p>
+          When I'm not dealing with code or reading, you'll most likely find me
+          either trying or modifying a recipe, volunteering somewhere, enjoying
+          a sci-fi show or film, or looking for a good rush that doesn't involve
+          bugs and a hard deadline.
+        </p>
+        <Link to='/resume' id='link'>
+          resumé
+        </Link>
       </div>
       <div id='cube-section'>
         <Cube />
