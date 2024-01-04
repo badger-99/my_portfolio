42df213cbd11f66759e7a1e824ebf5432ee30d17
Alfred Peter <alpeter91@gmail.com>
2024-01-04 20:56:58 +0300

    Fix syntax error

--- src/components/cube/Cube.jsx
+++ src/components/cube/Cube.jsx
@@ -5,27 +5,27 @@ const Cube = () => {
     <div id='cube-container'>
       <div className='cube-spinner'>
         <div className='face1'>
-          <i class='devicon-javascript-plain colored'></i>
+          <i className='devicon-javascript-plain colored'></i>
         </div>
 
         <div className='face2'>
-          <i class='devicon-react-original colored'></i>
+          <i className='devicon-react-original colored'></i>
         </div>
 
         <div className='face3'>
-          <i class='devicon-redux-original colored'></i>
+          <i className='devicon-redux-original colored'></i>
         </div>
 
         <div className='face4'>
-          <i class='devicon-ruby-plain colored'></i>
+          <i className='devicon-ruby-plain colored'></i>
         </div>
 
         <div className='face5'>
-          <i class='devicon-rails-plain colored'></i>
+          <i className='devicon-rails-plain colored'></i>
         </div>
 
         <div className='face6'>
-          <i class='devicon-postgresql-plain colored'></i>
+          <i className='devicon-postgresql-plain colored'></i>
         </div>
       </div>
     </div>
