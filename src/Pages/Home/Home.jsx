fb8fad1bec6e0ff34bd967096ab7a7d1adf58894
Alfred Peter <alpeter91@gmail.com>
2024-01-06 00:25:39 +0300

    Update homepage styling

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import my_picture from '../../assets/my_picture.jpg'
+import my_picture from '../../assets/my_picture.jpg';
 import './home.scss';
 
 const Home = () => {
@@ -8,20 +8,31 @@ const Home = () => {
       <div id='my_picture'>
         <img src={my_picture} alt='My Picture' />
       </div>
-      <div>
+      <div id='hero-text'>
         <div id='intro'>
           <h1>Hi,</h1>
           <h1>I'm Alfred.</h1>
-          <h2>A Full Stack Developer.</h2>
+          {/* <h2>A Full Stack Developer.</h2> */}
         </div>
-        <br />
-        <p>
-          I can help you build a product, feature, or website. Feel free to look
-          through some of
-          <Link to='/portfolio'> my work</Link>, and if you like what you see
-          and have a project you need coded, don't hesitate to{' '}
-          <Link to='/contact'> contact me</Link>.
-        </p>
+        <div id='typing-svg'>
+          <a href='https://git.io/typing-svg'>
+            <img
+              src='https://readme-typing-svg.demolab.com?font=DM+Mono&weight=500&size=28&duration=4000&pause=1000&color=FFFFFF&center=true&random=false&width=375&lines=Front-end+Fixer;Back-end+Engineer;Full+Stack+Developer'
+              alt='Typing SVG'
+            />
+          </a>
+        </div>
+        <div id='links'>
+          <Link className='hero-link' to='/about'>
+            about me
+          </Link>
+          <Link className='hero-link' to='/portfolio'>
+            my work
+          </Link>
+          <Link className='hero-link' to='/contact'>
+            talk to me
+          </Link>
+        </div>
       </div>
     </section>
   );
