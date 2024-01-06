675349447ce0e4617511264c11e836026807fda3
Alfred Peter <alpeter91@gmail.com>
2024-01-07 01:53:34 +0300

    Scale up home page for progressively larger screens

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,30 @@
 import { Link } from 'react-router-dom';
 import my_picture from '../../assets/my_picture.jpg';
 import './home.scss';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setScreenWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  const typingSvgSm =
+    'https://readme-typing-svg.demolab.com?font=DM+Mono&weight=500&size=28&duration=4000&pause=1000&color=FFFFFF&center=true&random=false&width=335&lines=Front-end+Fixer;Back-end+Builder;Full+Stack+Developer';
+
+  const typingSvgMed =
+    'https://readme-typing-svg.demolab.com?font=DM+Mono&weight=500&size=32&duration=4000&pause=1000&color=FFFFFF&center=true&random=false&width=395&lines=Front-end+Fixer;Back-end+Builder;Full+Stack+Developer';
+
+  const typingSvgLg =
+    'https://readme-typing-svg.demolab.com?font=DM+Mono&weight=500&size=37&duration=4000&pause=1000&color=FFFFFF&random=false&width=450&height=66&lines=Front-end+Fixer;Back-end+Builder;Full+Stack+Developer';
+
   return (
     <section className='pages' id='home'>
       <div id='my_picture'>
@@ -17,7 +39,9 @@ const Home = () => {
         <div id='typing-svg'>
           <a href='https://git.io/typing-svg'>
             <img
-              src='https://readme-typing-svg.demolab.com?font=DM+Mono&weight=500&size=28&duration=4000&pause=1000&color=FFFFFF&center=true&random=false&width=375&lines=Front-end+Fixer;Back-end+Builder;Full+Stack+Developer'
+              src={
+                screenWidth < 476 ? typingSvgSm : screenWidth < 900 ? typingSvgMed : typingSvgLg
+              }
               alt='Typing SVG'
             />
           </a>
