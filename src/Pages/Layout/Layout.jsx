0247c58bd0d96224e61f7d6616f4dc0990a7139b
Alfred Peter <alpeter91@gmail.com>
2024-01-30 14:11:59 +0300

    Fix navigation scrolling bug

--- src/Pages/Layout/Layout.jsx
+++ src/Pages/Layout/Layout.jsx
@@ -1,11 +1,25 @@
-import { Outlet } from "react-router-dom";
-import './layout.scss'
+import { useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+import './layout.scss';
 
 const Layout = () => {
+  const listenToNavigate = () => {
+    window.scrollTo(0, 0);
+  }
+
+  useEffect(() => {
+    const fixScroll = listenToNavigate;
+
+    return () => {
+      fixScroll();
+    };
+
+  });
+
   return (
     <section id='layout'>
       <Outlet />
     </section>
-  )
-}
-export default Layout
+  );
+};
+export default Layout;
